a76e2611408279f7e72a04d790ebf0a228aeb75c
Rommel Nicolás Zambrano Gaona <84996374+RommelZambrano@users.noreply.github.com>
2022-08-23 21:43:20 -0500

    added project minimarket-technology

---
+++ CODIGOS PROYECTO/Sistema Ventas Minimarket Technology  (V.1)/GRUPO1/src/sistemaventa/SistemaVenta.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemaventa;
+
+import Vista.Login;
+import Vista.Registro;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class SistemaVenta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Registro lg = new Registro();
+        lg.setVisible(true);
+    }
+    
+}
